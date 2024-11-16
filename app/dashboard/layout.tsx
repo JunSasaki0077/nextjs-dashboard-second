60e9ebb15bbf61b362ed579df17e4716d8fee65b
Jun Sasaki <ikasas0077@gmail.com>
2024-11-17 06:28:06 +0900

    5章

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,10 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
+import { ReactNode } from "react";
+import SideNav from "../ui/dashboard/sidenav";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+      <div className="w-full flex md:w-64">
         <SideNav />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
--- app/lib/settings.ts
+++
@@ -1,7 +0,0 @@
-export type Invoice = {
-  id: string;
-  customer_id: string;
-  amount: number;
-  status: 'pending' | 'paid';
-  date: string;
-};
