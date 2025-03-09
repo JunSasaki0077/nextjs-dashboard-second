336b1492943d83cbb2e0af33f94e2cd61b2f4995
Jun Sasaki <ikasas0077@gmail.com>
2025-03-09 22:44:44 +0900

    14章

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,7 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons'
 import { lusitana } from '@/app/ui/fonts'
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
 import { Suspense } from 'react'
-import { NuqsAdapter } from 'nuqs/adapters/next/app'
+
 import { fetchInvoicesPages } from '@/app/lib/data'
 
 export default async function Page(props: {
